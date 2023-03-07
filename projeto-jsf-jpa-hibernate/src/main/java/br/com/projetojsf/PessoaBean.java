21ba810a7eb78f131b7b8b7c53491929e5152f31
Daniel Penelva <d4n.andrade@gmail.com>
2023-03-07 02:52:17 -0300

    Criando projeto jsf-jpa-hibernate. Este projeto é a cópia do projeto-jsf + novas bibliotecas no maven

---
+++ projeto-jsf-jpa-hibernate/src/main/java/br/com/projetojsf/PessoaBean.java
@@ -0,0 +1,90 @@
+package br.com.projetojsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+
+@ViewScoped
+@ManagedBean(name = "pessoaBean")
+public class PessoaBean {
+
+	private String nome;
+	private String senha;
+	private String texto;
+
+	private HtmlCommandButton commandButton;
+
+	private List<String> nomes = new ArrayList<String>();
+	private List<String> listaCadastro = new ArrayList<String>();
+
+	public String addNome() {
+		nomes.add(nome);
+
+		if (nomes.size() > 3) {
+			commandButton.setDisabled(true);
+			return "paginanavegada?faces-redirect=true";
+		}
+
+		return ""; // null ou vazia fica na mesma página --> outcome
+	}
+
+	public String addListaCadastro() {
+		listaCadastro.add(nome);
+		listaCadastro.add(senha);
+		listaCadastro.add(texto);
+
+		return "";
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
+
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public List<String> getListaCadastro() {
+		return listaCadastro;
+	}
+
+	public void setListaCadastro(List<String> listaCadastro) {
+		this.listaCadastro = listaCadastro;
+	}
+
+}
